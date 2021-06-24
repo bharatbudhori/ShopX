9ae0381956562400b454a52ccf0152ea0470de77
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2021-06-24 17:18:11 +0530

    Setting up google mob ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shop_x/services/admob_services.dart';
 import 'package:shop_x/views/welcome_screen.dart';
 
 //import './views/homepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  AdMobService.initialize();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
