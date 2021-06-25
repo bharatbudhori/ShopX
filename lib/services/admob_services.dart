1f3506dabfcaecbfc3352fe7a1696f866e1ec42f
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2021-06-25 17:40:01 +0530

    Interstitial Ads

--- lib/services/admob_services.dart
+++ lib/services/admob_services.dart
@@ -7,6 +7,13 @@ class AdMobService {
       ? 'ca-app-pub-3940256099942544/6300978111'
       : 'ca-app-pub-3940256099942544/6300978111';
 
+  static String get interstitialAdUnitId => Platform.isAndroid
+      ? '	ca-app-pub-3940256099942544/1033173712'
+      : '	ca-app-pub-3940256099942544/1033173712';
+
+  InterstitialAd _interstitialAd;
+  int numberOfAttemptLoad = 0;
+
   static initialize() {
     if (MobileAds.instance == null) {
       MobileAds.instance.initialize();
@@ -18,14 +25,53 @@ class AdMobService {
       size: AdSize.largeBanner,
       adUnitId: bannerAdUnitId,
       listener: BannerAdListener(
-        onAdClosed: (Ad ad) => print('Add lodded'),
+        onAdClosed: (Ad ad) => print('Ad closed'),
         onAdFailedToLoad: (Ad ad, LoadAdError error) {
           ad.dispose();
         },
-        onAdOpened: (Ad ad) => print('Adopened'),
+        onAdOpened: (Ad ad) => print('Ad opened'),
+        onAdLoaded: (Ad ad) => print('Add lodded'),
       ),
       request: AdRequest(),
     );
     return ad;
+  }
+
+  void createInterad() {
+    InterstitialAd.load(
+      adUnitId: 'ca-app-pub-3940256099942544/1033173712',
+      request: AdRequest(),
+      adLoadCallback:
+          InterstitialAdLoadCallback(onAdLoaded: (InterstitialAd ad) {
+        _interstitialAd = ad;
+        numberOfAttemptLoad = 0;
+      }, onAdFailedToLoad: (LoadAdError error) {
+        numberOfAttemptLoad++;
+        _interstitialAd = null;
+        if (numberOfAttemptLoad <= 2) {
+          createInterad();
+        }
+      }),
+    );
+  }
+
+// show interstitial ads to user
+  void showInterad() {
+    if (_interstitialAd == null) {
+      return;
+    }
+    _interstitialAd.fullScreenContentCallback = FullScreenContentCallback(
+        onAdShowedFullScreenContent: (InterstitialAd ad) {
+      print("ad onAdshowedFullscreen");
+    }, onAdDismissedFullScreenContent: (InterstitialAd ad) {
+      print("ad Disposed");
+      ad.dispose();
+    }, onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError aderror) {
+      print('$ad OnAdFailed $aderror');
+      ad.dispose();
+      createInterad();
+    });
+    _interstitialAd.show();
+    _interstitialAd = null;
   }
 }
